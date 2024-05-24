963cff5f77b6a63be560683f05b91356f6e0a83e
samynjonas <jonas@otiv.ai>
2024-05-24 11:23:33 +0200

    Uploaded base for documentation webpage
    The documentation for the simulator will be put on a webpage hosted by github -- these are the base files which can and will be changed

---
+++ OtivAirsim/docusaurus.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Otiv Simulator',
+  tagline: 'documentation',
+  favicon: 'img/OTIV.jpg',
+
+  // Set the production url of your site here
+  url: 'https://otiv-jonas-samyn.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/OTIV-Simulator/',
+  
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'OTIV', // Usually your GitHub org/user name.
+  projectName: 'OTIV-Simulator', // Usually your repo name.
+  trailingSlash: false,
+  
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/OTIV.jpg',
+      navbar: {
+        title: 'Otiv Simulator',
+        logo: {
+          alt: 'OTIV logo',
+          src: 'img/OTIV.jpg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'documentationSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/OTIVai/OTIV-Simulator',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Project',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/Simulator/Simulator',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/OTIVai/OTIV-Simulator',
+              }           
+            ],
+          },
+          {
+            title: 'Socials',
+            items: [ 
+              {
+                label: 'OTIV',
+                href: 'https://www.otiv.ai',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/otivai',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/otiv/posts/?feedView=all',
+              },              
+              {
+                label: 'Instagram',
+                href: 'https://www.instagram.com/otiv_ai/',
+              }             
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Simulator Documentation Page. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ OtivAirsim/src/pages/TestPage.md
@@ -0,0 +1,7 @@
+---
+title: TestPage
+---
+
+# Test page example
+
+This is a test page and will be used to test out the different page options
